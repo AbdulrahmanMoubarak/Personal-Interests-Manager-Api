e694b5be9c0a981d07b4a353a890be7304f99396
Abdulrahman Moubarak <83316370+AbdulrahmanMoubarak@users.noreply.github.com>
2022-03-13 20:02:06 +0200

    Update ProjectModels.py
    Updated MovieModel & added model for list items and model for Section

--- Personal Interests Manger Server/ProjectModels.py
+++ Personal Interests Manger Server/ProjectModels.py
@@ -2,28 +2,29 @@ class MovieModel:
     def __init__(self, params=[]):
         self.adult = params[0]
         self.belong_to_collection = params[1]
-        self.budget = params[2]
-        self.genres = params[3]
-        self.movie_id = params[4]
-        self.original_language = params[5]
-        self.overview = params[6]
-        self.popularity = params[7]
-        self.poster = params[8]
-        self.production_company = params[9]
-        self.release_date = params[10]
-        self.runtime = params[11]
-        self.status = params[12]
-        self.tag_line = params[13]
-        self.title = params[14]
-        self.vote_average = params[15]
-        self.vote_count = params[16]
+        self.genres = params[2]
+        self.movie_id = params[3]
+        self.original_language = params[4]
+        self.overview = params[5]
+        self.popularity = params[6]
+        self.poster = params[7]
+        self.production_company = params[8]
+        self.release_date = params[9]
+        self.status = params[10]
+        self.tag_line = params[11]
+        self.title = params[12]
+        self.vote_average = params[13]
+        self.vote_count = params[14]
+        self.imdb_id = params[15]
+        self.year = params[16]
+        self.trailer = params[17]
+        self.background = params[18]
 
     def to_dict(movie):
         if isinstance(movie, MovieModel):
             dict = {
                 "adult": movie.adult,
                 "belong_to_collection": movie.belong_to_collection,
-                "budget": movie.budget,
                 "genres": movie.genres,
                 "movie_id": movie.movie_id,
                 "original_language": movie.original_language,
@@ -32,12 +33,14 @@ class MovieModel:
                 "poster": movie.poster,
                 "production_company": movie.production_company,
                 "release_date": movie.release_date,
-                "runtime": movie.runtime,
                 "status": movie.status,
                 "tag_line": movie.tag_line,
                 "title": movie.title,
                 "vote_average": movie.vote_average,
                 "vote_count": movie.vote_count,
+                "imdb_id": movie.imdb_id,
+                "trailer": movie.trailer,
+                "background": movie.background
             }
             return dict
         else:
@@ -70,4 +73,38 @@ class MovieModel:
         else:
             type_name = book.__class__.__name__
             raise TypeError("Unexpected type {0}".format(type_name))
+            
+class MediaItemPartialModel:
+    def __init__(self, id, name, image):
+        self.id = id
+        self.name = name
+        self.image = image
+
+    def to_dict(item):
+        if isinstance(item, MediaItemPartialModel):
+            return {
+                "item_id": item.id,
+                "item_name": item.name,
+                "image": item.image
+            }
+        else:
+            type_name = item.__class__.__name__
+            raise TypeError("Unexpected type {0}".format(type_name))
+
+
+class SectionModel:
+    def __init__(self, sectionName, itemList=[]):
+        self.sectionName = sectionName
+        self.itemList = itemList
+
+    def to_dict(section):
+        if isinstance(section, SectionModel):
+            return {
+                "section_name": section.sectionName,
+                "items": section.itemList,
+            }
+        else:
+            type_name = section.__class__.__name__
+            raise TypeError("Unexpected type {0}".format(type_name))
+
 
