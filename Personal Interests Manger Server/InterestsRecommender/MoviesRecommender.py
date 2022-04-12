6ad09b2ba79dff38a51d5efc7d9e2d3fa8130c4b
Abdulrahman Moubarak <83316370+AbdulrahmanMoubarak@users.noreply.github.com>
2022-04-12 13:10:45 +0200

    Update MoviesRecommender.py
    Added some improvements

--- Personal Interests Manger Server/InterestsRecommender/MoviesRecommender.py
+++ Personal Interests Manger Server/InterestsRecommender/MoviesRecommender.py
@@ -5,18 +5,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import sqlite3
 
-conn = sqlite3.connect("pim_database.db")
 
-class Movie:
+class MovieRecommender:
 
-    def ContentWithUserId(self,userId):
+    def ContentWithUserId(self, userId, conn):
         df = pd.read_sql_query("""Select genres,movie_id from movies_metadata""", conn)
         df2 = pd.read_sql_query("""Select movie_id from movie_rating where user_id=""" + str(userId), conn)
         merged = pd.merge(df, df2)
         for movie_id in merged['movie_id'].values:
             self.Contentbased(movie_id, df)
 
-    def CollabWithUserId(self, userId):
+    def CollabWithUserId(self, userId, conn):
         df = pd.read_sql_query("""Select movie_id , title from movies_metadata""", conn)
         df2 = pd.read_sql_query("""Select movie_id,rating from movie_rating where user_id=""" + str(userId), conn)
         df3 = pd.read_sql_query("""Select user_id,movie_id,rating from movie_rating""", conn)
@@ -25,9 +24,9 @@ class Movie:
         df3['movie_id'] = df3['movie_id'].astype(str)
         merged = pd.merge(df, df2)
         merged2 = pd.merge(df, df3)
-        self.CollabBased(merged,merged2)
+        return self.CollabBased(merged, merged2)
 
-    def Contentbased(self,movieid, df):
+    def Contentbased(self, movieid, df, conn):
         id = movieid
         ind = df[df['movie_id'] == id].index.values[0]
         row_genre = df.at[ind, 'genres']
@@ -60,10 +59,10 @@ class Movie:
             indices = pd.Series(chunk.index, index=chunk['movie_id']).drop_duplicates()
 
             # Function that takes in movie title as input and outputs most similar movies
-            temp=temp.append(self.__recommd(id,cosine_sim,indices,chunk))
-        print(temp.to_frame())
+            temp = temp.append(self.__recommd(id, cosine_sim, indices, chunk))
+        return temp.to_frame()
 
-    def __recommd(self,title,cosine_sim,indices,chunk):
+    def __recommd(self, title, cosine_sim, indices, chunk):
 
         # Get the index of the movie that matches the title
         idx = indices[title]
@@ -80,11 +79,11 @@ class Movie:
         # Get the movie indices
         movie_indices = [i[0] for i in sim_scores]
 
-        v=chunk['movie_id'].iloc[movie_indices]
+        v = chunk['movie_id'].iloc[movie_indices]
         # Return the top 10 most similar movies
         return v
 
-    def CollabBased(self,merged,merged2):
+    def CollabBased(self, merged, merged2):
         # make table where the index is userId and coloums is Book title and the values are rating
         user_rating = merged2.pivot_table(index='user_id', columns='movie_id', values='rating')
 
@@ -94,21 +93,18 @@ class Movie:
         # apply pearson rule for standaralization
         movie_similarity = user_rating.corr(method='pearson')
 
-        # function for getting recommendation
-        def recommend_movie(movie_id, user_rating):
-            if movie_id in movie_similarity:
-                movie_score = movie_similarity[movie_id] * (user_rating - 2.5)
-                movie_score = movie_score.sort_values(ascending=False)
-                return movie_score
-
         similar_movie = pd.DataFrame()
 
         for index, row in merged.iterrows():
-            similar_movie = similar_movie.append(recommend_movie(row["movie_id"], row["rating"]), ignore_index=True)
+            similar_movie = similar_movie.append(self.__recommend_movie(row["movie_id"], row["rating"], movie_similarity), ignore_index=True)
 
         # similar_movie.sum().sort_values(ascending=False)
 
-        print(similar_movie.sum().sort_values(ascending=False))
+        return similar_movie.sum().sort_values(ascending=False)
 
-r=Movie()
-r.ContentWithUserId(46)
+        # function for getting recommendation
+    def __recommend_movie(self, movie_id, user_rating, movie_similarity):
+        if movie_id in movie_similarity:
+            movie_score = movie_similarity[movie_id] * (user_rating - 2.5)
+            movie_score = movie_score.sort_values(ascending=False)
+            return movie_score
