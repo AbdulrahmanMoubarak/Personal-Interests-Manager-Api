19586068d1ad6d568b408a52db2a9ac007ec6a18
Abdulrahman Moubarak <83316370+AbdulrahmanMoubarak@users.noreply.github.com>
2022-04-19 13:52:00 +0200

    Update SongRecommender.py

--- Personal Interests Manger Server/InterestsRecommender/SongRecommender.py
+++ Personal Interests Manger Server/InterestsRecommender/SongRecommender.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import sqlite3
-conn = sqlite3.connect("pim_database.db")
-
-class Song:
-
-    def ContentWithSongId(self,SongId):
-        conn = sqlite3.connect("pim_database.db")
-        df = pd.read_sql_query("""Select song_id , danceability, energy,loudness,tempo from songs_metadata""", conn)
-        df2 = pd.read_sql_query("""Select song_id , danceability, energy,loudness,tempo from songs_metadata where song_id="""+str(SongId), conn)
-        return self.ContentBased(df,df2)
-
-    def ContentBased(self,df,df2):
-        df=abs(df)
-        df2=abs(df2)
-        df["tempo"]=abs(df["tempo"]-df2["tempo"].values)
-        df["energy"]=abs(df["energy"]-df2["energy"].values)
-        df["danceability"]=abs(df["danceability"]-df2["danceability"].values)
-        df["loudness"]=abs(df["loudness"]-df2["loudness"].values)
-        df=df.sort_values(by=["tempo","energy","danceability","loudness"], ascending=True)
-        return df['song_id']
 
 
-r=Song()
-print(r.ContentWithSongId(56))
-#56
+class SongRecommender:
+
+    def ContentWithSongId(self, SongId, conn):
+        df = pd.read_sql_query("""SELECT song_spotify_id, danceability, energy, loudness, tempo FROM songs_metadata""",
+                               conn)
+        df2 = pd.read_sql_query(
+            """SELECT song_spotify_id, danceability, energy, loudness, tempo FROM songs_metadata WHERE song_spotify_id=(?)""", conn, params=[SongId])
+        return self.__ContentBased(df, df2)
+
+    def __ContentBased(self, df, df2):
+        df["tempo"] = abs(abs(df["tempo"]) - abs(df2["tempo"].values))
+        df["energy"] = abs(abs(df["energy"]) - abs(df2["energy"].values))
+        df["danceability"] = abs(abs(df["danceability"]) - abs(df2["danceability"].values))
+        df["loudness"] = abs(abs(df["loudness"]) - abs(df2["loudness"].values))
+        df = df.sort_values(by=["tempo", "energy", "danceability", "loudness"], ascending=True)
+        subDf = df.loc[(df['tempo'] > float(0)) | (df['energy'] > float(0)) | (df['danceability'] > float(0)) | (df['loudness'] > float(0))]
+        ret = subDf['song_spotify_id'].tolist()
+        return ret[:40]
